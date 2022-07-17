530bd98da4423300ff44f4bde364f4741d53efe3
Angela.Duran <45835464+AngelaDuran@users.noreply.github.com>
2022-07-17 17:37:04 -0400

    Line 156 comment

--- detect.py
+++ detect.py
@@ -153,15 +153,15 @@ def run(
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    #s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        #with open(f'{txt_path}.txt', 'a') as f:
-                            #f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                        with open(f'{txt_path}.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
